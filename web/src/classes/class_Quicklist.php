1e2d881b3225d93c72a5fa1fb9e3a1d9dd719c42
mal1k <00154@ukr.net>
2021-08-25 16:14:54 +0300

    .

--- web/src/classes/class_Quicklist.php
+++ web/src/classes/class_Quicklist.php
@@ -152,8 +152,7 @@
 					$sql = "SELECT item_id FROM Quicklist WHERE account_id = $acc AND item_type = '".$from."'";
 				}
 
-				$result = $dbObj->Query("SELECT 123");
-				return $result;
+				$result = $dbObj->Query($sql);
 
 				unset($items);
 				while ($row = mysqli_fetch_array($result)) {
